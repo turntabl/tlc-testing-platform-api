ff47a004c90680640cba80bbfe6f0ebde40dac09
Nicholas-TurnTabl <nicholas.dickson@turntabl.io>
2020-12-03 12:32:24 +0000

    update : added postgresql dependency

--- src/main/java/com/turntabl/testsystem/TestSystemApplication.java
+++ src/main/java/com/turntabl/testsystem/TestSystemApplication.java
@@ -2,8 +2,9 @@ package com.turntabl.testsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class TestSystemApplication {
 
 	public static void main(String[] args) {
