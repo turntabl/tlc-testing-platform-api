bd998fea5b2464eaffbc5707e121846e25b8779e
Nicholas-TurnTabl <nicholas.dickson@turntabl.io>
2020-12-03 12:17:35 +0000

    project setup : first commit

---
+++ src/test/java/com/turntabl/testsystem/TestSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turntabl.testsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
