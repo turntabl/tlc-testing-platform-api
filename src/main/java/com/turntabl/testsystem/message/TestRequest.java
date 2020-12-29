c17201542ea8cd6095fde13081212ffb23eaf2b3
despitegfx <despitegfx@gmail.com>
2020-12-29 15:44:15 +0000

    test rule

--- src/main/java/com/turntabl/testsystem/message/TestRequest.java
+++ src/main/java/com/turntabl/testsystem/message/TestRequest.java
@@ -5,7 +5,7 @@ public class TestRequest {
     private long test_id;
     private String test_title;
     private String questions_type;
-    private String test_rules;
+    private String test_rule;
     private String test_date;
     private String test_time_start;
     private String test_time_end;
@@ -15,7 +15,7 @@ public class TestRequest {
         this.course_id = course_id;
         this.test_id = test_id;
         this.test_title = test_title;
-        this.test_rules = test_rules;
+        this.test_rule = test_rules;
         this.test_date = test_date;
         this.test_time_start = test_time_start;
         this.test_time_end = test_time_end;
@@ -38,11 +38,11 @@ public class TestRequest {
     public void setTest_title(String test_title) {
         this.test_title = test_title;
     }
-    public String getTest_rules() {
-        return test_rules;
+    public String getTest_rule() {
+        return test_rule;
     }
-    public void setTest_rules(String test_rules) {
-        this.test_rules = test_rules;
+    public void setTest_rule(String test_rules) {
+        this.test_rule = test_rules;
     }
     public String getTest_date() {
         return test_date;
