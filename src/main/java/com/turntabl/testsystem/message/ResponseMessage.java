b67dea6aa36e66c9ed2c5e3ae289cacda9783fdf
Nicholas-TurnTabl <nicholas.dickson@turntabl.io>
2021-01-04 15:23:48 +0000

    Update ResponseMessage.java

--- src/main/java/com/turntabl/testsystem/message/ResponseMessage.java
+++ src/main/java/com/turntabl/testsystem/message/ResponseMessage.java
@@ -1,27 +1,32 @@
 package com.turntabl.testsystem.message;
-public class ResponseMessage {
+public class ResponseMessage<T> {
     private String message;
     private int status_code;
-    private AddStudentSaveResponse addStudentSaveResponse;
+    private T data;
     public ResponseMessage() {
     }
-    public ResponseMessage(String message, int status_code, AddStudentSaveResponse addStudentSaveResponse) {
+
+    public ResponseMessage(String message, int status_code, T t) {
         this.message = message;
         this.status_code = status_code;
-        this.addStudentSaveResponse = addStudentSaveResponse;
+        this.data = t;
     }
+
     public int getStatus_code() {
         return status_code;
     }
     public void setStatus_code(int status_code) {
         this.status_code = status_code;
     }
-    public AddStudentSaveResponse getAddStudentSaveResponse() {
-        return addStudentSaveResponse;
+
+    public T getT() {
+        return data;
     }
-    public void setAddStudentSaveResponse(AddStudentSaveResponse addStudentSaveResponse) {
-        this.addStudentSaveResponse = addStudentSaveResponse;
+
+    public void setT(T t) {
+        this.data = t;
     }
+
     public String getMessage() {
         return message;
     }
